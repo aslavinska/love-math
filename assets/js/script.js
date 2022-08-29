37fb15f87813dcb58144d066dadc383fee6f50f1
aslavinska <anastasija.slavinska@avl.com>
2022-08-29 09:55:32 +0000

    Add code to calculate random numbers for the dame questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,23 @@
 
-function runGame(){
+document.addEventListener("DOMContentLoaded", function(){
 
+    let buttons = documeny.getElementByTagName("button");
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            if (this.getAttribute ("data-type")==="submit"){
+                alert("you clicked Submit!");
+            } else{
+                let gameType = this.getAttribute("data-type");
+                alert(`you clicked $(gameType)`);
+            }
+        })
+    }
+
+})
+
+function runGame(){
+    let num1 = Math.floor(Math.random()=25)+1;
+    let num2 = Math.floor(Math.random()=25)+1;
 }
 
 function checkAnswer(){
@@ -28,6 +45,6 @@ function displaySubtractQuestion(){
 }
 
 function displayMultiplyQuestion(){
-    
+
 }
 
